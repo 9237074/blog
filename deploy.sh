335f6e5541ad22e25f967d65f339f6cfe9f56322
9237074 <9237074@qq.com>
2021-04-16 23:47:01 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-# cd .vuepress/dist
+cd .vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -19,4 +19,4 @@ git commit -m 'deploy'
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
-git push
+git push -f git@github.com:9237074/blog.igt master:gh-pages
